baecdb893beaa7675e7a7fda83053b1512ae1b16
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-26 12:35:10 -0800

    Add a print board fn

--- src/tictactoe/game.clj
+++ src/tictactoe/game.clj
@@ -1,5 +1,12 @@
 (ns tictactoe.game
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.pprint :as pp]))
+
+(defn print-board [board]
+  (pp/cl-format *out* "~:{+---+---+---+~%~
+                          | ~C | ~C | ~C |~%~}~
+                          +---+---+---+~%"
+                (partition 3 board)))
 
 
 (defn -main [& args]
