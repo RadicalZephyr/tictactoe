9a6d2f7a419151cad166e1a5e1f3d46507cb81b9
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-26 21:25:51 -0800

    Add some tests for winning-move

---
+++ test/tictactoe/ai_test.clj
@@ -0,0 +1,33 @@
+(ns tictactoe.ai-test
+  (:require [tictactoe.ai :refer :all]
+            [clojure.test :refer :all]))
+
+(deftest winning-move-test
+  (testing "False positives"
+    (is (and
+         (= (winning-move '(" " "x" " "))
+            nil)
+         (= (winning-move '(" " "o" " "))
+            nil))
+        "Two spaces and a mark are not a winning move.")
+
+    (is (and
+         (= (winning-move '("o" "o" "x"))
+            nil)
+         (= (winning-move '("x" "x" "o"))
+            nil)
+         (= (winning-move '("x" "o" "o"))
+            nil)
+         (= (winning-move '("o" "x" "x"))
+            nil)
+         (= (winning-move '("o" "x" "x"))
+            nil))
+        "No spaces mean it's not a winning move."))
+
+  (testing "True positives"
+    (is (= (winning-move '(" " "x" "x"))
+           "x"))
+    (is (= (winning-move '("x" " " "x"))
+           "x"))
+    (is (= (winning-move '("x" "x" " "))
+           "x"))))
