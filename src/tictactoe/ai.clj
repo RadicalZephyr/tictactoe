767c4f69ce7ec46df494814aaa9cf3812ec310fa
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-26 16:23:11 -0800

    Stub out AI

--- src/tictactoe/ai.clj
+++ src/tictactoe/ai.clj
@@ -1,5 +1,12 @@
 (ns tictactoe.ai)
 
+(defn- has-winning-move? [board])
 
+(defn- play-winning-move [board])
 
-(defn best-move [board])
+(defn- play-best-available-move [board])
+
+(defn best-move [board]
+  (cond
+    (has-winning-move? board) (play-winning-move board)
+    :else (play-best-available-move board)))
