917f217906609d5d09b23d72dc47cc8648041c85
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-27 13:48:37 -0800

    Implement the simplest "next best move" function

--- src/tictactoe/ai.clj
+++ src/tictactoe/ai.clj
@@ -41,9 +41,27 @@
   (when-let [move (get-winning-move board mark)]
     (board/make-move board mark move)))
 
-(defn- play-best-available-move [board marks])
+(def move-ranking
+  "The priority ordering of all the different moves in the
+  game. Basically, the corners are superior to the middle which is
+  superior to the side moves.  This has to do with the number of
+  potential paths that each space contributes to.  Corners contribute
+  three potential paths, the middle has four, and the sides have two
+  each."
+  [[1 1] [1 3] [3 1] [3 3] [2 2] [1 2] [2 1] [3 2] [2 3]])
+
+(defn not-taken [board pos]
+  (when (= (board/get-pos board pos)
+           " ")
+    pos))
+
+(defn- play-best-available-move [board mark]
+  (->>
+   move-ranking
+   (some (partial not-taken board))
+   (board/make-move board mark)))
 
 (defn best-move [board marks]
   (let [my-mark (marks :ai)]
       (or (play-winning-move board my-mark)
-          (play-best-available-move board marks))))
+          (play-best-available-move board my-mark))))
