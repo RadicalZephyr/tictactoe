61c568efc12637dc59d3f929325b63e1aebe9243
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-27 15:24:54 -0800

    Put null check last

--- src/tictactoe/attack.clj
+++ src/tictactoe/attack.clj
@@ -4,12 +4,12 @@
   (let [marks (vec (distinct attack))]
     (cond
       (apply = " " marks)                      :potential
-      (let [c (count marks)]
-       (or (= c
-              3)
-           (and (= c
-                   2)
-                (not (contains? marks " "))))) :null
       (every? #{" " my-mark} marks)            :shot
       (every? #{" " other-mark} marks)         :threat
-      :else                                    :unknown)))
+      (let [c (count marks)]
+        (or (= c
+               3)
+            (and (= c
+                    2)
+                 (not (contains? marks " "))))) :null
+      :else                                     :unknown)))
