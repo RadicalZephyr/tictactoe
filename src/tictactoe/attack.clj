1e1fd2cc339cef2cd54c3772c9c73d224db41ad7
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-27 15:28:22 -0800

    Use a set in classify

--- src/tictactoe/attack.clj
+++ src/tictactoe/attack.clj
@@ -1,15 +1,11 @@
 (ns tictactoe.attack)
 
 (defn classify [attack my-mark other-mark]
-  (let [marks (vec (distinct attack))]
+  (let [marks (set attack)]
     (cond
-      (apply = " " marks)                      :potential
-      (every? #{" " my-mark} marks)            :shot
-      (every? #{" " other-mark} marks)         :threat
-      (let [c (count marks)]
-        (or (= c
-               3)
-            (and (= c
-                    2)
-                 (not (contains? marks " "))))) :null
-      :else                                     :unknown)))
+      (= #{" "}            marks) :potential
+      (= #{" " my-mark}    marks) :shot
+      (= #{" " other-mark} marks) :threat
+      (or (= #{" " my-mark other-mark} marks)
+          (= #{my-mark other-mark}     marks)) :null
+      :else                                    :unknown)))
