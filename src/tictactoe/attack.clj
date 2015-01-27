8a6f70d682943e67cb6ecbdf08150685cfde2994
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-27 15:20:51 -0800

    Tighten up null test

--- src/tictactoe/attack.clj
+++ src/tictactoe/attack.clj
@@ -4,9 +4,12 @@
   (let [marks (vec (distinct attack))]
     (cond
       (apply = " " marks)              :potential
-      (or (= (count marks)
-             3)
-          (not (contains? marks " "))) :null
+      (let [c (count marks)]
+       (or (= c
+              3)
+           (and (= c
+                   2)
+                (not (contains? marks " "))))) :null
       (and (contains? marks " ")
            (contains? marks my-mark))  :shot
       (and (= (count marks)
