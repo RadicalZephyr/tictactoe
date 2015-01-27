6deeba88e2934a2df05ddedff690bbd02288e6c6
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-27 13:52:23 -0800

    Refactor AI to work with cli expectations

--- src/tictactoe/ai.clj
+++ src/tictactoe/ai.clj
@@ -37,10 +37,6 @@
                        first
                        first)))))
 
-(defn- play-winning-move [board mark]
-  (when-let [move (get-winning-move board mark)]
-    (board/make-move board mark move)))
-
 (def move-ranking
   "The priority ordering of all the different moves in the
   game. Basically, the corners are superior to the middle which is
@@ -55,13 +51,11 @@
            " ")
     pos))
 
-(defn- play-best-available-move [board mark]
-  (->>
-   move-ranking
-   (some (partial not-taken board))
-   (board/make-move board mark)))
+(defn- get-best-available-move [board mark]
+  (some (partial not-taken board)
+        move-ranking))
 
 (defn best-move [board marks]
   (let [my-mark (marks :ai)]
-      (or (play-winning-move board my-mark)
-          (play-best-available-move board my-mark))))
+      (or (get-winning-move board my-mark)
+          (get-best-available-move board my-mark))))
