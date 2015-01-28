a79bcc13fab664600c7a21c20e2d0729709688ba
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-27 19:16:27 -0800

    Outline basic seesaw structure

--- src/tictactoe/gui.clj
+++ src/tictactoe/gui.clj
@@ -1,7 +1,12 @@
 (ns tictactoe.gui
-  (:require [seesaw.core :as s]))
+  (:require  [tictactoe.ai :as ai]
+             [tictactoe.board :as board]
+             [seesaw.core :as s]
+             [seesaw.bind :as b]))
 
 (def root (atom nil))
+
+(def board (atom board/empty-board))
 
 (defn get-canvas []
   (s/select @root [:#canvas]))
@@ -11,9 +16,19 @@
    (-> frame
        s/show!)))
 
+(defn draw-board [c g])
+
+
+(defn mouse-click [e])
+
 (defn -main [& args]
   (compare-and-set!
    root nil (s/frame :title "Tic-Tac-Toe"
                      :size [640 :by 480]
-                     :content (s/canvas :id :canvas)))
-  (show-frame @root))
+                     :content (s/canvas :id :canvas
+                                        :paint draw-board)))
+  (show-frame @root)
+  (b/bind board (b/b-do [_]
+                    (s/repaint! (get-canvas))))
+  (s/listen (get-canvas)
+      :mouse-clicked mouse-click))
