c390b5650c6954964a6450cf47dfa60c233ee942
Geoff Shannon <geoffpshannon@gmail.com>
2015-01-28 17:59:45 -0800

    Add command line switches for differentiating gui and cli

--- src/tictactoe/game.clj
+++ src/tictactoe/game.clj
@@ -1,6 +1,21 @@
 (ns tictactoe.game
   (:gen-class)
-  (:require [tictactoe.cli :as cli]))
+  (:require [tictactoe.cli :as cli]
+            [tictactoe.gui :as gui]))
+
+(defn print-usage []
+  (println "tictactoe: [interface]\n")
+  (println "Play a friendly game of tictactoe against a completely")
+  (println "unbeatable AI. Don't feel bad though. No one can beat it.")
+  (println "  -h --help\tPrint this message")
+  (println "  -g --gui\tGraphical interface. Complete with button(s)!")
+  (println "  -c --cli\tCommand-line/text interface.  No buttons though."))
 
 (defn -main [& args]
-  (cli/start-game))
+  (if (seq args)
+    (case (first args)
+      ("-h" "--help") (print-usage)
+      ("-g" "--gui")  (gui/-main)
+      ("-c" "--cli")  (cli/start-game)
+      (print-usage))
+    (cli/start-game)))
