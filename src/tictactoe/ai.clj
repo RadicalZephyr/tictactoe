c484446e81ea1596234584ceefd9e30b04cffa18
Geoff Shannon <geoffpshannon@gmail.com>
2015-02-08 18:12:24 -0500

    Add pseudo-code for minimax

--- src/tictactoe/ai.clj
+++ src/tictactoe/ai.clj
@@ -94,3 +94,23 @@
      first
      ((fn [[pos ranking]]
         pos)))))
+
+
+;; function minimax(node, depth, maximizingPlayer)
+;;   if depth = 0 or node is a terminal node
+;;     return the heuristic value of node
+;;   if maximizingPlayer
+;;     bestValue := -∞
+;;     for each child of node
+;;       val := minimax(child, depth - 1, FALSE)
+;;       bestValue := max(bestValue, val)
+;;     return bestValue
+;;   else
+;;     bestValue := +∞
+;;     for each child of node
+;;       val := minimax(child, depth - 1, TRUE)
+;;       bestValue := min(bestValue, val)
+;;     return bestValue
+
+
+(defn minimax [board depth maximizing-player])
