ebadd4eb4d85375d25a890c64b5f8d19937a7599
Geoff Shannon <geoffpshannon@gmail.com>
2015-02-13 12:45:49 -0500

    Remove snapshot for release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject tictactoe "0.2.0-SNAPSHOT"
+(defproject tictactoe "0.2.0"
   :description "A tictactoe game with an UNBEATABLE AI."
   :url "http://www.zephyrizing.net/tictactoe/"
   :license {:name "Eclipse Public License"
