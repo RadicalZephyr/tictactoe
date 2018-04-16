768da745fd9c1f7265c87215d897a1acd0bf4f40
Geoff Shannon <geoffpshannon@gmail.com>
2018-04-15 23:20:48 -0700

    Update dependency versions

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://www.zephyrizing.net/tictactoe/"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.reader "0.8.13"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/tools.reader "1.2.2"]
                  [seesaw "1.4.5"]]
   :main ^:skip-aot tictactoe.game
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[org.clojure/math.combinatorics "0.0.8"]
-                                  [org.clojure/test.check "0.7.0"]]}})
+             :dev {:dependencies [[org.clojure/math.combinatorics "0.1.4"]
+                                  [org.clojure/test.check "0.9.0"]]}})
